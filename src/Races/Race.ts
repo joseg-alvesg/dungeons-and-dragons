9e006e6bb06a06842aadda32168dd3551d3327f6
joseg-alvesg <cinhoalves85@gmail.com>
2023-06-05 15:16:53 -0300

    implements: Races

---
+++ src/Races/Race.ts
@@ -0,0 +1,49 @@
+abstract class Race {
+  private readonly _name: string;
+  private readonly _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  abstract get maxLifePoints(): number;
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances() {
+    throw new Error('Not implemented');
+  }
+}
+
+// abstract class Race {
+//   private readonly _name: string;
+//   private readonly _dexterity: number;
+
+//   constructor(name: string, dexterity: number) {
+//     this._name = name;
+//     this._dexterity = dexterity;
+//   }
+
+//   static createdRacesInstances() {
+//     throw new Error('Not implemented');
+//   }
+
+//   abstract get maxLifePoints(): number;
+
+//   name(): string {
+//     return this._name;
+//   }
+
+//   dexterity(): number {
+//     return this._dexterity;
+//   }
+// }
+
+export default Race;
