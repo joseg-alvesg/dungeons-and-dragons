9d0326c3eb265b1395c7809c5f819226591e0a01
joseg-alvesg <cinhoalves85@gmail.com>
2023-06-05 16:26:41 -0300

    fix: `createdArchetype` func exports

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -23,7 +23,7 @@ export default abstract class Archetype {
     return this._cost;
   }
 
-  static createdArchetypesInstances() {
+  static createdArchetypeInstances() {
     throw new Error('Not implemented');
   }
 
